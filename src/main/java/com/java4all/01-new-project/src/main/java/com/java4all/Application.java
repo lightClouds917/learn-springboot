96168ab8e641753f1f8942aae9036052d377b00e
unknown <you@example.com>
2018-07-03 19:55:24 +0800

    init ..

---
+++ src/main/java/com/java4all/01-new-project/src/main/java/com/java4all/Application.java
@@ -0,0 +1,21 @@
+package com.java4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping(value = "login",method = RequestMethod.GET)
+	public String login(){
+		return "欢迎进入Spring Boot的世界";
+	}
+}
